7c37f7ac1ccda47928801f8c57e2bb1e407dca47
Pasquale Boemio <boemianrapsodi@gmail.com>
2025-12-12 07:56:15 +0100

    fix: added hls.js to support hls playback on chrome

--- workspaces/@pul.se/web/components/video/index.js
+++ workspaces/@pul.se/web/components/video/index.js
@@ -1,11 +1,33 @@
-import { wrapper, video_element } from './styles.module.css';
+import { useLayoutEffect, useRef } from 'react';;
+import { Hls } from 'hls.js';
+
+import { wrapper } from './styles.module.css';
 
 export default function Video({ url }) {
+  const ref = useRef();
+
+  useLayoutEffect(function() {
+    if(ref.current == null) {
+      return;
+    }
+
+    const video = ref.current;
+    const hls = new Hls();
+
+    hls.on(Hls.Events.MEDIA_ATTACHED, function () {
+      video.play().catch(function() {
+        video.muted = true;
+        video.play();
+      });
+    });
+
+    hls.loadSource(url);
+    hls.attachMedia(video);
+  }, [ ref, url ]);
+
   return (
     <div className={ wrapper }>
-      <video controls className={ video_element } autoplay>
-        <source src={ url } type='application/x-mpegURL' />
-      </video>
+      <video ref={ ref } controls />
     </div>
   );
 };
